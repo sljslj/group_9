080e3f3ea7c9a13de160dc174b871cb9685532ef
sljslj <552033298@qq.com>
2018-12-31 01:13:46 +0800

    remove README

--- Group_9_1/cuckoo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# admin.site.register(models.User)
-admin.site.register(models.Favorite)
-admin.site.register(models.SearchInput)
-admin.site.register(models.SearchResult)
-admin.site.register(models.History)
-admin.site.register(models.EmailVerifyRecord)
--- Group_9_1/cuckoo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CuckooConfig(AppConfig):
-    name = 'cuckoo'
